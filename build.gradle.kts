330892379eeef2c850eed2de5724efcce54b8fff
Adam Cameron <cameron.adam+github@gmail.com>
2022-10-09 15:52:03 +0100

    TDDing a web service

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
-val kotest_version: String by project
-val system_lambda_version: String by project
-val commons_lang_3_version: String by project
-val junit_jupiter_version: String by project
-val ktor_client_core_version: String by project
+val kotestVersion: String by project
+val systemLambdaVersion: String by project
+val commonsLang3Version: String by project
+val junitJupiterVersion: String by project
+val ktorVersion: String by project
+val exposedVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization").version("1.7.10")
     application
 }
 
@@ -18,15 +20,20 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    testImplementation("io.kotest:kotest-runner-junit5:$kotest_version")
-    testImplementation("io.kotest:kotest-assertions-core:$kotest_version")
-    testImplementation("io.kotest:kotest-framework-datatest:$kotest_version")
-    testImplementation("com.github.stefanbirkner:system-lambda:$system_lambda_version")
-    testImplementation("org.apache.commons:commons-lang3:$commons_lang_3_version")
-    testImplementation("org.junit.jupiter:junit-jupiter:$junit_jupiter_version")
-    testImplementation("org.junit.jupiter:junit-jupiter:$junit_jupiter_version")
-    implementation("io.ktor:ktor-client-core:$ktor_client_core_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_client_core_version")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-framework-datatest:$kotestVersion")
+    testImplementation("com.github.stefanbirkner:system-lambda:$systemLambdaVersion")
+    testImplementation("org.apache.commons:commons-lang3:$commonsLang3Version")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 }
 
 tasks.withType<Test>().configureEach {
--- src/test/kotlin/junit/practical/WebServiceTest.kt
+++ src/test/kotlin/junit/practical/WebServiceTest.kt
@@ -3,20 +3,89 @@ package junit.practical
 import io.kotest.common.runBlocking
 import io.kotest.matchers.shouldBe
 import io.ktor.client.HttpClient
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import io.ktor.client.request.get
-import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.Serializable
+
 
 @DisplayName("Tests of WebService class")
 internal class WebServiceTest {
 
+    val webserviceUrl = "http://localhost:8080/numbers/"
+
+    @Serializable
+    data class Number(val id: Int, val en: String, val mi: String)
+
     @Test
-    fun `it returns a 200`() {
+    fun `It should return a 200-OK on the root URI on a valid request`() {
         runBlocking {
             HttpClient().use { client ->
-                val response = client.get("https://jsonplaceholder.typicode.com/todos/1")
+                val response = client.get(webserviceUrl) {
+                    header("Accept", "application/json")
+                }
+                
                 response.status shouldBe HttpStatusCode.OK
+            }
+        }
+    }
+    @Test
+    fun `It should return a 406-NOT-ACCEPTABLE and suggest the expected type if the Accepts header is not application-json`() {
+        runBlocking {
+            HttpClient().use { client ->
+                val response = client.get(webserviceUrl) {
+                    header("Accept", "text/plain")
+                }
+
+                response.status shouldBe HttpStatusCode.NotAcceptable
+                response.body() as String shouldBe """["application/json"]"""
+            }
+        }
+    }
+    @Test
+    fun `It returns an array of Numbers as a JSON array`() {
+        runBlocking {
+            HttpClient() {
+                install(ContentNegotiation) {
+                    json()
+                }
+            }.use { client ->
+                val response = client.get(webserviceUrl) {
+                    header("Accept", "application/json")
+                }
+
+                response.status shouldBe HttpStatusCode.OK
+                response.body() as List<Number> shouldBe listOf(
+                    Number(1, "one", "tahi"),
+                    Number(2, "two", "rua"),
+                    Number(3, "three", "toru"),
+                    Number(4, "four", "wha")
+                )
+            }
+        }
+    }
+    @Test
+    fun `It will accept a POST request of an object as JSON and return the same object as confirmation, and its URL`() {
+        val five = Number(5, "five", "rima")
+        runBlocking {
+            HttpClient() {
+                install(ContentNegotiation) {
+                    json()
+                }
+            }.use { client ->
+                val response = client.post(webserviceUrl) {
+                    contentType(ContentType.Application.Json)
+                    setBody(five)
+                }
+
+                response.status shouldBe HttpStatusCode.Created
+                response.body() as Number shouldBe five
+                response.headers["Location"] shouldBe "${webserviceUrl}5"
             }
         }
     }
